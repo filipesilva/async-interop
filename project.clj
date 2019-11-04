00da73fd9d00f0d61c2299ce068deffc22bd7e82
Filipe Silva <filipematossilva@gmail.com>
2019-11-04 14:52:46 +0000

    publish to clojars

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject async-interop "0.1.0"
+  :description "Promise interop for core.async"
+  :url "https://github.com/filipesilva/async-interop"
+  :license {:name "MIT"
+            :url "https://github.com/filipesilva/async-interop/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojurescript "1.10.520" :scope "provided"]]
+  :source-paths ["src/main"]
+  :repositories {"clojars" {:url "https://clojars.org/repo"
+                            :sign-releases false}})
